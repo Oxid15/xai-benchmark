e3edff53e332eea4630491956ea25cf81f961e51
Ilia Moiseev <villeman.5@yandex.ru>
2022-10-15 23:05:59 +0300

    Add random baseline explainer

---
+++ xaib/explainers/random_explainer.py
@@ -0,0 +1,12 @@
+from typing import Any
+import numpy as np
+from ..base import Model, Explainer
+
+
+class RandomExplainer(Explainer):
+    def __init__(self, n_features, *args, meta_prefix=None, **kwargs) -> None:
+        super().__init__(*args, meta_prefix=meta_prefix, **kwargs)
+        self.n_features = n_features
+    
+    def predict(self, x: Any, model: Model) -> Any:
+        return np.random.random((len(x), self.n_features))
