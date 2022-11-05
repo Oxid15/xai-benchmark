84e4cadc38a356d5978ae3e04635823c5c5f4406
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-11-05 21:41:10 +0300

    Add shap explainer prototype

---
+++ xaib/explainers/shap_explainer.py
@@ -0,0 +1,15 @@
+import numpy as np
+import shap
+
+from ..base import Explainer
+
+
+class ShapExplainer(Explainer):
+    def __init__(self, train_ds, *args, meta_prefix=None, **kwargs) -> None:
+        super().__init__(*args, meta_prefix=meta_prefix, **kwargs)
+        self._train_ds = train_ds
+
+    def predict(self, x, model):
+        explainer = shap.Explainer(model.predict,
+            np.array([item for item in self._train_ds]))
+        return explainer([x]).data[0]
