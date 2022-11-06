7cca7d4e5148d4654e0d97960076011e63aaf39b
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-11-06 13:37:47 +0300

    Restructure the project to use several explanation types

--- xaib/cases/__init__.py
+++
@@ -1,4 +0,0 @@
-from .continuity_case import ContinuityCase
-from .contrastivity_case import ContrastivityCase
-from .correctness_case import CorrectnessCase
-from .coherence_case import CoherenceCase
---
+++ xaib/cases/feature_importance/__init__.py
@@ -0,0 +1,4 @@
+from .continuity_case import ContinuityCase
+from .contrastivity_case import ContrastivityCase
+from .correctness_case import CorrectnessCase
+from .coherence_case import CoherenceCase
