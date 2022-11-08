Correct RMSE calculation when using batches

xaib/cases/feature_importance/coherence_case.py
@@ -4,7 +4,7 @@ import numpy as np
 from tqdm import tqdm
 
 from ...base import Case, Explainer, Model, Dataset
-from ...utils import rmse, SimpleDataloader
+from ...utils import batch_rmse, SimpleDataloader
 
 
 class CoherenceCase(Case):
@@ -32,9 +32,8 @@ class CoherenceCase(Case):
             other_e = [other_expl.predict(item, self._model, **other_kwargs) 
                 for other_expl, other_kwargs in zip(expls, expls_kwargs)]
 
-            d = [rmse(e, oe) for oe in other_e]
-            d = np.mean(d)
-            diffs.append(d)
+            for oe in other_e:
+                diffs += batch_rmse(e, oe)
 
         self.metrics['coherence'] = {
             'other_disagreement': np.nanmean(diffs)

xaib/cases/feature_importance/continuity_case.py
@@ -4,14 +4,14 @@ import numpy as np
 from tqdm import tqdm
 
 from ...base import Case, Explainer, Model, Dataset
-from ...utils import rmse, SimpleDataloader
+from ...utils import batch_rmse, SimpleDataloader
 
 
 class ContinuityCase(Case):
     """
     Apply noise of small magnitude to the input data.
     Obtain original and perturbed explanations.
-    Compare them using **MSE or RMSE** and average.
+    Compare them using RMSE and average.
     """
     def __init__(self, ds: Dataset, noisy_ds: Dataset, model: Model) -> None:
         super().__init__(ds, model)
@@ -36,7 +36,7 @@ class ContinuityCase(Case):
             explanation = expl.predict(item, self._model, **expl_kwargs)
             noisy_explanation = expl.predict(noisy_item, self._model, **expl_kwargs)
 
-            rmses.append(rmse(explanation, noisy_explanation))
+            rmses += batch_rmse(explanation, noisy_explanation)
 
         self.metrics['continuity'] = {
             'small_noise_check': np.nanmean(rmses)

xaib/cases/feature_importance/contrastivity_case.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from xaib.utils import rmse
+from xaib.utils import batch_rmse
 from ...base import Case, Explainer
 
 
@@ -14,12 +14,15 @@ class ContrastivityCase(Case):
 
                 # Calculate the RMSE between this explanation and
                 # others of other classes
-                d = [rmse(e, explanations[x][i]) for x in range(len(coords)) if x != u]
-                d = np.mean(d)
-                diffs.append(d)
+                d = []
+                for x in range(len(coords)):
+                    if x != u:
+                        d += batch_rmse(e, explanations[x][i])
+                diffs += d
         return diffs
 
     def evaluate(self, expl: Explainer) -> None:
+        # TODO: refactor the process
         labels = np.asarray([item['label'] for item in self._ds])
         unique, counts = np.unique(labels, return_counts=True)
         min_len = np.min(counts)

xaib/cases/feature_importance/correctness_case.py
@@ -1,7 +1,7 @@
 import numpy as np
 from tqdm import tqdm
 from ...base import Case, Explainer, Dataset, Model
-from ...utils import rmse, SimpleDataloader
+from ...utils import batch_rmse, SimpleDataloader
 
 
 class CorrectnessCase(Case):
@@ -20,10 +20,10 @@ class CorrectnessCase(Case):
         for batch in tqdm(SimpleDataloader(self._ds, batch_size)):
             item = batch['item']
 
-            e = expl.predict(item, self._model, **expl_kwargs)
-            ne = expl.predict(item, self._noisy_model, **expl_noisy_kwargs)
+            explanation_batch = expl.predict(item, self._model, **expl_kwargs)
+            noisy_explanation_batch = expl.predict(item, self._noisy_model, **expl_noisy_kwargs)
 
-            diffs_expl += [rmse(rowe, rowne) for (rowe, rowne) in zip(e, ne)]
+            diffs_expl += batch_rmse(explanation_batch, noisy_explanation_batch)
 
         self.metrics['correctness'] = {
                 'parameter_randomization_check': np.nanmean(diffs_expl)