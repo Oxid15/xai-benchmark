766cab4a117be913f41c55bc64ac60b236fc062b
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-11-08 17:53:23 +0300

    Contrastivity can't use batches in the same sense - made it compatible

--- xaib/cases/feature_importance/contrastivity_case.py
+++ xaib/cases/feature_importance/contrastivity_case.py
@@ -1,15 +1,10 @@
 import numpy as np
 
 from xaib.utils import rmse
-from ...base import Dataset, Model, Case, Explainer
+from ...base import Case, Explainer
 
 
 class ContrastivityCase(Case):
-    def __init__(
-        self, ds: Dataset, model: Model, *args, **kwargs) -> None:
-
-        super().__init__(ds, model, *args, **kwargs)
-
     def _compare(self, coords, explanations):
         diffs = []
 
@@ -22,22 +17,24 @@ class ContrastivityCase(Case):
                 d = [rmse(e, explanations[x][i]) for x in range(len(coords)) if x != u]
                 d = np.mean(d)
                 diffs.append(d)
-        return np.mean(diffs)
+        return diffs
 
     def evaluate(self, expl: Explainer) -> None:
-        labels = np.asarray([label for _, label in self.ds])
+        labels = np.asarray([item['label'] for item in self._ds])
         unique, counts = np.unique(labels, return_counts=True)
         min_len = np.min(counts)
 
-        unique_coords = [np.argwhere(labels == ul)[:min_len] for ul in unique]
+        # List of coordinates in ds for every unique label
+        coords = [np.argwhere(labels == ul)[:min_len] for ul in unique]
 
-        explanations = [[] for u in range(len(unique_coords))]
-        for u in range(len(unique_coords)):
-            for i in range(len(unique_coords[u])):
-                item, _ = self.ds[unique_coords[u][i][0]]
-                e = expl.predict(item, self.model)
+        explanations = [[] for _ in range(len(coords))]
+        for u in range(len(coords)):
+            for i in range(len(coords[u])):
+                item = self._ds[coords[u][i][0]]
+                e = expl.predict([item['item']], self._model)
                 explanations[u].append(e)
 
+        diffs = self._compare(coords, explanations)
         self.metrics['contrastivity'] = {
-            'label_difference': self._compare(unique_coords, explanations)
+            'label_difference': np.nanmean(diffs)
         }
