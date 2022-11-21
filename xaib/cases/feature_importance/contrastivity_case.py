4c450e3b78864c9a15ad6f2f27d614d9471d3430
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-11-21 18:37:45 +0300

    Refactor ContrastivityCase

--- xaib/cases/feature_importance/contrastivity_case.py
+++ xaib/cases/feature_importance/contrastivity_case.py
@@ -1,43 +1,59 @@
-import numpy as np
+from typing import Iterable
 
-from xaib.utils import batch_rmse
+from tqdm import tqdm
+import numpy as np
+from cascade.data import Sampler
+
+from ...utils import batch_rmse, SimpleDataloader
 from ...base import Case, Explainer
 
 
+class Filter(Sampler):
+    def __init__(self, ds, indices: Iterable[int], **kwargs) -> None:
+        super().__init__(ds, num_samples=len(indices), **kwargs)
+
+        self._indices = indices
+    
+    def __getitem__(self, index):
+        return self._dataset[self._indices[index]]
+
+
 class ContrastivityCase(Case):
-    def _compare(self, coords, explanations):
-        diffs = []
+    def evaluate(self, expl: Explainer, batch_size: int = 1) -> None:
+        # Obtain all the labels
+        labels = np.asarray([item['label'] for item in
+            tqdm(self._ds, desc='Obtaining labels', leave=False)])
 
-        for u in range(len(coords)):
-            for i in range(len(coords[u])):
-                e = explanations[u][i]
-
-                # Calculate the RMSE between this explanation and
-                # others of other classes
-                d = []
-                for x in range(len(coords)):
-                    if x != u:
-                        d += batch_rmse(e, explanations[x][i])
-                diffs += d
-        return diffs
-
-    def evaluate(self, expl: Explainer) -> None:
-        # TODO: refactor the process
-        labels = np.asarray([item['label'] for item in self._ds])
-        unique, counts = np.unique(labels, return_counts=True)
+        # Determine how much of an intersection different labels have
+        unique_labels, counts = np.unique(labels, return_counts=True)
         min_len = np.min(counts)
 
-        # List of coordinates in ds for every unique label
-        coords = [np.argwhere(labels == ul)[:min_len] for ul in unique]
+        # For each label get indexes of its items
+        coords = {u: np.argwhere(labels == u)[0][:min_len] for u in unique_labels}
 
-        explanations = [[] for _ in range(len(coords))]
-        for u in range(len(coords)):
-            for i in range(len(coords[u])):
-                item = self._ds[coords[u][i][0]]
-                e = expl.predict([item['item']], self._model)
-                explanations[u].append(e)
+        # Obtain all explanations by batches
+        explanations = {u: [] for u in unique_labels}
+        for u in unique_labels:
+            dl = SimpleDataloader(Filter(self._ds, coords[u]), batch_size=batch_size)
+            for batch in tqdm(dl):
+                explanations[u].append(expl.predict(batch['item'], self._model))
 
-        diffs = self._compare(coords, explanations)
+        # Compare explanations of different labels
+        diffs = {u: [] for u in unique_labels}
+        for u in unique_labels:
+            for other_label in unique_labels:
+                if u == other_label:
+                    continue
+
+                for batch, other_batch in zip(
+                    explanations[u],
+                    explanations[other_label]
+                ):
+                    rmse = batch_rmse(batch, other_batch)
+                    diffs[u] += list(rmse)
+        for u in unique_labels:
+            diffs[u] = np.nanmean(diffs[u])
+
         self.metrics['contrastivity'] = {
-            'label_difference': np.nanmean(diffs)
+            'label_difference': np.nanmean([diffs[u] for u in diffs])
         }
