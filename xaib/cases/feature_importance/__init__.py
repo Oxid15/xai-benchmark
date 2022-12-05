80e828eaf4da6781aa038d69b42dea55a6b6dc3a
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2022-12-05 20:18:26 +0300

    Add computation

--- xaib/cases/feature_importance/__init__.py
+++ xaib/cases/feature_importance/__init__.py
@@ -2,3 +2,4 @@ from .continuity_case import ContinuityCase
 from .contrastivity_case import ContrastivityCase
 from .correctness_case import CorrectnessCase
 from .coherence_case import CoherenceCase
+from .compactness_case import CompactnessCase
