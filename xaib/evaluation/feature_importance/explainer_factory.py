7ec14331882f0040c19ba9ed518233085b276c48
Ilia Moiseev <villeman.5@yandex.ru>
2023-03-16 11:26:42 +0300

    Pass labels to lime

--- xaib/evaluation/feature_importance/explainer_factory.py
+++ xaib/evaluation/feature_importance/explainer_factory.py
@@ -6,9 +6,9 @@ from ...explainers.feature_importance.lime_explainer import LimeExplainer
 
 
 class ExplainerFactory(Factory):
-    def __init__(self, train_ds: Dataset, model: Model) -> None:
+    def __init__(self, train_ds: Dataset, model: Model, labels=[0, 1]) -> None:
         super().__init__()
         self._constructors['const'] = lambda: ConstantExplainer(constant=1)
         self._constructors['random'] = lambda: RandomExplainer(shift=-15, magnitude=10)
         self._constructors['shap'] = lambda: ShapExplainer(train_ds)
-        self._constructors['lime'] = lambda: LimeExplainer(train_ds, labels=(0, 1))
+        self._constructors['lime'] = lambda: LimeExplainer(train_ds, labels=labels)
