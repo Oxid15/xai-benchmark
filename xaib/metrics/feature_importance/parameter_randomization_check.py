629bb988e5e2e522764c77e41c4ec394e68b48a5
Ilia Moiseev <villeman.5@yandex.ru>
2023-04-01 17:58:57 +0300

    Fix formatting and direction of metric

--- xaib/metrics/feature_importance/parameter_randomization_check.py
+++ xaib/metrics/feature_importance/parameter_randomization_check.py
@@ -11,18 +11,21 @@ class ParameterRandomizationCheck(Metric):
     to the underlying model - whether it is
     sensitive to the changes in model
     """
-    def __init__(self, ds: Dataset, model: Model, noisy_model: Model, **kwargs: Any) -> None:
+
+    def __init__(
+        self, ds: Dataset, model: Model, noisy_model: Model, **kwargs: Any
+    ) -> None:
         super().__init__(ds, model, **kwargs)
         self._noisy_model = noisy_model
-        self.name = 'parameter_randomization_check'
-        self.direction = 'up'
+        self.name = "parameter_randomization_check"
+        self.direction = "down"
 
     def compute(
         self,
         expl: Explainer,
         batch_size: int = 1,
         expl_kwargs: Union[Dict[Any, Any], None] = None,
-        expl_noisy_kwargs: Union[Dict[Any, Any], None] = None
+        expl_noisy_kwargs: Union[Dict[Any, Any], None] = None,
     ) -> None:
         if expl_kwargs is None:
             expl_kwargs = {}
@@ -32,10 +35,12 @@ class ParameterRandomizationCheck(Metric):
         diffs_expl = []
 
         for batch in tqdm(SimpleDataloader(self._ds, batch_size)):
-            item = batch['item']
+            item = batch["item"]
 
             explanation_batch = expl.predict(item, self._model, **expl_kwargs)
-            noisy_explanation_batch = expl.predict(item, self._noisy_model, **expl_noisy_kwargs)
+            noisy_explanation_batch = expl.predict(
+                item, self._noisy_model, **expl_noisy_kwargs
+            )
 
             explanation_batch = minmax_normalize(explanation_batch)
             noisy_explanation_batch = minmax_normalize(noisy_explanation_batch)
