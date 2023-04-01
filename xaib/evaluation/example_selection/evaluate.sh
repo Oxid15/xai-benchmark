74fb5d18aeeedf4bd07b8676782f11aabaa89cbc
Ilia Moiseev <villeman.5@yandex.ru>
2023-04-01 20:46:23 +0300

    New evaluation everywhere

--- xaib/evaluation/example_selection/evaluate.sh
+++ xaib/evaluation/example_selection/evaluate.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-python3 make_dataset.py
-python train_knn_model.py
 python example_selection.py
--- xaib/evaluation/feature_importance/evaluate.sh
+++ xaib/evaluation/feature_importance/evaluate.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-python3 make_dataset.py
-python train_model.py
 python feature_importance.py
