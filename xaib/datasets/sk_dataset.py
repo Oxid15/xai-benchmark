6b93365e14dff687bad0c6844b157a3ca7d0c3b1
Моисеев Илья Юрьевич <villeman.5@yandex.ru>
2023-04-18 00:36:04 +0300

    Add some more new datasets including real-life ones; Restrict eval size

--- xaib/datasets/sk_dataset.py
+++ xaib/datasets/sk_dataset.py
@@ -2,8 +2,21 @@ from typing import Any, Dict
 
 import numpy as np
 from cascade import data as cdd
-from sklearn.datasets import load_iris, load_digits
+from sklearn.datasets import (load_iris, load_digits, load_breast_cancer, fetch_covtype,
+                              fetch_kddcup99)
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
+
+
+def prepare_kddcup99():
+    x, y = fetch_kddcup99(return_X_y=True) # 1, 2, 3 - categorical
+
+    enc = OneHotEncoder(sparse_output=False)
+    cats = enc.fit_transform(x[:, 1:3])
+    x = np.concatenate((x[:, 0].reshape(len(x), 1), cats, x[:, 4:]), axis=1)
+    y = OrdinalEncoder().fit_transform(y.reshape(len(y), 1))
+
+    return x, y
 
 
 class SkDataset(cdd.SizedDataset):
@@ -15,6 +28,9 @@ class SkDataset(cdd.SizedDataset):
         constructors = {
             "iris": load_iris(return_X_y=True),
             "digits": load_digits(return_X_y=True),
+            "breast_cancer": load_breast_cancer(return_X_y=True),
+            "covtype": fetch_covtype(return_X_y=True),
+            'kddcup99': prepare_kddcup99()
         }
 
         if name not in constructors:
