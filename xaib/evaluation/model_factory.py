c9972d0fa18d8ef88b22bd8dfbd4675e6c867e68
Ilia Moiseev <villeman.5@yandex.ru>
2023-04-29 13:15:43 +0300

    Add standard nn model

--- xaib/evaluation/model_factory.py
+++ xaib/evaluation/model_factory.py
@@ -2,6 +2,7 @@ import numpy as np
 from cascade.utils.sk_model import SkModel
 from sklearn.metrics import f1_score
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.svm import SVC
 
 from ..base import Factory
@@ -44,8 +45,24 @@ def knn(train_ds, test_ds):
     return model
 
 
+def nn(train_ds, test_ds):
+    X_train, Y_train = [x["item"] for x in train_ds], [x["label"] for x in train_ds]
+    X_test, Y_test = [x["item"] for x in test_ds], [x["label"] for x in test_ds]
+
+    X_train = np.array(X_train)
+    Y_train = np.array(Y_train, dtype=int)
+    X_test = np.array(X_test)
+    Y_test = np.array(Y_test, dtype=int)
+
+    model = SkWrapper(blocks=[MLPClassifier()], name="nn")
+    model.fit(X_train, Y_train)
+    model.evaluate(X_test, Y_test, {"f1": lambda x, y: f1_score(x, y, average="macro")})
+    return model
+
+
 class ModelFactory(Factory):
     def __init__(self, train_ds, test_ds) -> None:
         super().__init__()
         self._constructors["svm"] = lambda: svm(train_ds, test_ds)
         self._constructors["knn"] = lambda: knn(train_ds, test_ds)
+        self._constructors["nn"] = lambda: nn(train_ds, test_ds)
