21a3f488d621f98f61a1e8fc0488679bb04f998d
Ilia Moiseev <villeman.5@yandex.ru>
2023-05-01 19:17:25 +0300

    Write more data in meta of data

--- xaib/datasets/sk_dataset.py
+++ xaib/datasets/sk_dataset.py
@@ -1,4 +1,5 @@
 from typing import Any, Dict
+from cascade.base import PipeMeta
 
 import numpy as np
 from cascade import data as cdd
@@ -12,10 +13,12 @@ from sklearn.model_selection import train_test_split
 
 
 class SkDataset(cdd.SizedDataset):
+    # TODO: this is classification dataset - should be renamed
     def __init__(self, name, split, frac=0.8, *args, **kwargs) -> None:
         super().__init__(**kwargs)
 
         self.name = name
+        self.split = split
 
         constructors = {
             "iris": load_iris(return_X_y=True),
@@ -44,3 +47,10 @@ class SkDataset(cdd.SizedDataset):
 
     def __getitem__(self, index: int) -> Dict[str, Any]:
         return {"item": self.x[index], "label": self.y[index]}
+
+    def get_meta(self):
+        meta = super().get_meta()
+        meta[0]["dataset_name"] = self.name
+        meta[0]["split"] = self.split
+        meta[0]["labels"] = self.labels
+        return meta
