bb0fd88e48e4d31697e27b1c2a4aec83e3032251
Ilia Moiseev <villeman.5@yandex.ru>
2023-05-01 19:13:08 +0300

    Evaluation with new setups

--- xaib/evaluation/feature_importance/feature_importance.py
+++ xaib/evaluation/feature_importance/feature_importance.py
@@ -1,20 +1,25 @@
 import matplotlib
 
 matplotlib.use("TkAgg")
+
 import os
 import sys
 
-from cascade import data as cdd
 from cascade.models import ModelRepo
 from xaib.evaluation import DatasetFactory, ModelFactory
-from xaib.evaluation.feature_importance import ExperimentFactory, ExplainerFactory
+from xaib.evaluation.feature_importance import (
+    ExperimentFactory,
+    ExplainerFactory,
+    CaseFactory,
+)
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(SCRIPT_DIR)
 PROJECT_DIR = os.path.dirname(os.path.dirname(BASE_DIR))
 sys.path.append(PROJECT_DIR)
 
-from xaib.evaluation.utils import visualize_results
+from xaib.evaluation.utils import visualize_results, Setup
+
 
 REPO_PATH = os.path.join(os.path.dirname(BASE_DIR), "results", "feature_importance")
 BS = 100
@@ -22,32 +27,35 @@ BS = 100
 # Overwrite previous run
 ModelRepo(REPO_PATH, overwrite=True)
 
-for dataset in [
-    "wine",
-    "breast_cancer",
-    "digits",
-    "iris",
-    "synthetic_noisy",
-    "synthetic",
-]:
-    for model in ["svm", "nn"]:
-        print(dataset, model)
+factories = (DatasetFactory(), ModelFactory(), ExplainerFactory(), CaseFactory())
+setups = [Setup(*factories, models_except="knn")]
 
-        train_ds, test_ds = DatasetFactory().get(dataset)
-        print(train_ds.get_meta())
+for setup in setups:
+    for dataset in setup.datasets:
+        for model in setup.models:
+            print(dataset, model)
 
-        labels = train_ds.labels
+            train_ds, test_ds = DatasetFactory().get(dataset)
+            print(train_ds.get_meta())
 
-        model = ModelFactory(train_ds, test_ds).get(model)
-        print(model.get_meta())
+            labels = train_ds.labels
 
-        explainers = ExplainerFactory(train_ds, model, labels=labels).get("all")
-        experiment_factory = ExperimentFactory(
-            REPO_PATH, explainers, test_ds, model, labels, BS
-        )
+            model = ModelFactory(train_ds, test_ds).get(model)
+            print(model.get_meta())
 
-        experiments = experiment_factory.get("all")
-        for name in experiments:
-            experiments[name]()
+            explainers = {
+                explainer: ExplainerFactory(train_ds, model, labels=labels).get(
+                    explainer
+                )
+                for explainer in setup.explainers
+            }
+
+            experiment_factory = ExperimentFactory(
+                REPO_PATH, explainers, test_ds, model, labels, BS
+            )
+
+            for case in setup.cases:
+                experiment = experiment_factory.get(case)
+                experiment()
 
 visualize_results(REPO_PATH, REPO_PATH)
